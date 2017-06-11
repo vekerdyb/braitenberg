f88f6dcaca03e7614710d8a25d71d02f4dfc7e34
Balint Vekerdy <balint@dangerfarms.com>
2017-06-11 18:59:17 +0100

    Make even better and nicer

--- main.js
+++ main.js
@@ -13,16 +13,71 @@ canvas.style.border = '1px solid blue';
 var heatMap = ctx.getImageData(0, 0, canvas.width, canvas.height);
 var data = heatMap.data;
 
-var frequency = canvas.width / 5;
+var frequency = canvas.width / 8;
+
+
+var hue2rgb = function hue2rgb(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
+};
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ */
+
+function hslToRgb(h, s, l) {
+    var r, g, b;
+
+    if (s === 0) {
+        r = g = b = l; // achromatic
+    } else {
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+var heatData = {};
+
+function addHeatData(x, y, value) {
+    heatData[x + ',' + y] = value;
+}
+
+function getHeatData(x, y) {
+    return heatData[x + ',' + y];
+}
 
 for (var x = 0; x < canvas.width; x++) {
     for (var y = 0; y < canvas.height; y++) {
-        var a = simplex.noise2D(x / frequency, y / frequency) * 0.6 + 0.8;
-        var b = simplex.noise2D(x / frequency, y / frequency) * 0.2 + 0.6;
-        data[(x + y * canvas.width) * 4] = 255;
+        var a = simplex.noise2D(x / frequency, y / frequency) * 0.9 + 0.8;
+        var b = simplex.noise2D(x / frequency, y / frequency);
+
+
+        a = 0.4 * a;
+        // https://stackoverflow.com/a/27263918
+        var rgb = hslToRgb(a, 1, 0.5);
+
+        addHeatData(x, y, a);
+
+        data[(x + y * canvas.width) * 4] = a * 255;
         data[(x + y * canvas.width) * 4 + 1] = 0;
-        data[(x + y * canvas.width) * 4 + 2] = 0;
-        data[(x + y * canvas.width) * 4 + 3] = 255 - a * 255 + b * 70;
+        data[(x + y * canvas.width) * 4 + 2] = (1 - a) * 255;
+        data[(x + y * canvas.width) * 4 + 3] = 255;
     }
 }
 
@@ -58,7 +113,7 @@ function Vehicle() {
     this.sl = 0;
     this.sr = 0;
     this.size = 10;
-    this.wheelDistance = 2; // todo
+    this.wheelDistance = 7; // todo
     this.x0 = randInt(canvas.width) - this.size;
     this.y0 = randInt(canvas.height) - this.size;
     this.x = this.x0;
@@ -74,17 +129,20 @@ function Vehicle() {
 
     this.activate = function (sensorValue1, sensorValue2) {
         // new speed
-        var leftMove = Math.exp(sensorValue1) * 2;
-        var rightMove = Math.exp(sensorValue2) * 2;
+        var leftMove = Math.exp(sensorValue1);
+        var rightMove = Math.exp(sensorValue2);
 
         _this.sl += leftMove;
         _this.sr += rightMove;
 
-        _this.x = Math.round((_this.getDistanceTravelled() + leftMove) * Math.cos(_this.theta) + _this.x0);
-
-        _this.y = Math.round((_this.getDistanceTravelled() + rightMove) * Math.sin(_this.theta) + _this.y0);
-
         _this.theta = (_this.sr - _this.sl) / _this.wheelDistance + _this.theta0;
+
+        var distance = _this.getDistanceTravelled();
+        _this.x = distance * Math.cos(_this.theta) + _this.x0;
+
+        _this.y = distance * Math.sin(_this.theta) + _this.y0;
+
+
     };
 
     this.reset = function () {
@@ -136,11 +194,11 @@ function step(timestamp) {
         showHeatmap(heatMap);
     }
 
-    for (var i = 0; i < numVehicles; i++) {
+    for (var i = 0; i < vehicles.length; i++) {
         var vehicle = vehicles[i];
         ctx.beginPath();
         ctx.arc(vehicle.x, vehicle.y, vehicle.size, 0, 2 * Math.PI, false);
-        ctx.fillStyle = "black";
+        ctx.fillStyle = "rgba(0,0,0,1)";
         ctx.fill();
         ctx.beginPath();
         ctx.strokeStyle = "white";
@@ -162,8 +220,8 @@ function step(timestamp) {
         // vehicle.theta = (vehicle.theta + 0.1) % (Math.PI * 2);
         vehicle.reset();
         vehicle.activate(
-            getChannel(heatMap.data, sensor1Pos.x, sensor1Pos.y, 3) / 255,
-            getChannel(heatMap.data, sensor2Pos.x, sensor2Pos.y, 3) / 255
+            getHeatData(sensor1Pos.x, sensor1Pos.y),
+            getHeatData(sensor2Pos.x, sensor2Pos.y)
         );
     }
     // stopped = true;
